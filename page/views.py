e68bec90744b52a1e4a863e800891648329dfcf9
xando <sebastian.pawlus@gmail.com>
2011-11-21 22:44:26 +0100

    import fix

--- page/views.py
+++ page/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from .page.models import *
+from pustkow.page.models import *
 
 def fotografie(request):
     return render_to_response('fotografie.html',
